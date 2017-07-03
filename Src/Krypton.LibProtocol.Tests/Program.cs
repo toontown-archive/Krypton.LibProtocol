fbf3ee2bf2a018426dc0304a1778ce615400aa83
Chandler Stowell <chandler14362@gmail.com>
2017-07-03 12:35:50 -0400

    tests and tool execution points
    need to configure xunit in the test project

---
+++ Src/Krypton.LibProtocol.Tests/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Krypton.LibProtocol.Tests
+{
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            var pf = new KryptonFile();
+            pf.Read("example.krypton");
+        }
+    }
+}
---
+++ Src/Krypton.LibProtocol.Tool/Src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Krypton.LibProtocol.Tool
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var s in args)
+            {
+                Console.Out.WriteLine(s);
+            }
+        }
+    }
+}
