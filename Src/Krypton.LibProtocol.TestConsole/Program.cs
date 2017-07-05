21ec7ab971366584e2a1b1d1acca0c15020b2052
Chandler Stowell <chandler14362@gmail.com>
2017-07-05 13:59:41 -0400

    minor file structure changes

---
+++ Src/Krypton.LibProtocol.TestConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using Krypton.LibProtocol.Target.CSharp;
+
+namespace Krypton.LibProtocol.TestConsole
+{
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            var pf = new KryptonFile();
+            pf.Read("Resources/groups.krypton");
+
+            var target = new CSharpTarget();
+            var context = target.Build(pf);
+            
+            var settings = new CSharpTargetSettings
+            {
+                Output = "TestOutput.cs"
+            };
+            context.Write(settings);
+        }
+    }
+}
