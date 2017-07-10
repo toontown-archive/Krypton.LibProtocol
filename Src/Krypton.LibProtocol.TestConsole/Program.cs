dfa65819804a737b12b388c8c6c106b3006d2cee
Chandler Stowell <chandler14362@gmail.com>
2017-07-10 15:07:24 -0400

    initial parser progress

--- Src/Krypton.LibProtocol.TestConsole/Program.cs
+++ Src/Krypton.LibProtocol.TestConsole/Program.cs
@@ -1,28 +1,12 @@
-﻿using Krypton.LibProtocol.Target.CSharp;
-
-namespace Krypton.LibProtocol.TestConsole
+﻿namespace Krypton.LibProtocol.TestConsole
 {
     public class Test
     {
         public static void Main(string[] args)
         {
             var pf = new KPDLFile();
-            pf.AddIncludeDirectory("Resources/");
+            pf.Includes.Directories.Add("Resources/");
             pf.Read("example.kpdl");
-
-            var target = new CSharpTarget();
-            var context = target.Build(pf);
-            
-            var settings = new CSharpTargetSettings
-            {
-                Output = "Gen",
-                Groups = new CSharpTargetSettings.GroupSettings
-                {
-                    Namespace = "Krypton.Protocol",
-                    ClassName = "Groups"
-                }
-            };
-            context.Write(settings);
         }
     }
 }
