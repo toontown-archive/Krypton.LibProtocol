724ae73577e075bbff0e5e66b7c1f615b9ebbe70
Chandler Stowell <chandler14362@gmail.com>
2017-07-24 12:43:24 -0400

    c#: have list type inherit list

--- Src/LibProtocol.Targets/CSharp/Src/Collections/ListType.cs
+++ Src/LibProtocol.Targets/CSharp/Src/Collections/ListType.cs
@@ -2,24 +2,12 @@
 
 namespace Krypton.LibProtocol.Collections
 {
-    public class ListType<TK> : IKryptonType where TK: IKryptonType, new()
+    public class ListType<TK> : List<TK>, IKryptonType where TK: IKryptonType, new()
     {
-        public List<TK> Value { get; set; }
-
-        public static implicit operator ListType<TK>(List<TK> val)
-        {
-            return new ListType<TK> { Value = val };
-        }
-        
-        public static implicit operator List<TK>(ListType<TK> val)
-        {
-            return val.Value;
-        }
-        
         public void Write(BufferWriter bw)
         {
-            bw.WriteUInt16((ushort)Value.Count);
-            foreach (var val in Value)
+            bw.WriteUInt16((ushort)Count);
+            foreach (var val in this)
             {
                 val.Write(bw);
             }
@@ -27,14 +15,14 @@ namespace Krypton.LibProtocol.Collections
 
         public void Consume(BufferReader br)
         {
-            Value = new List<TK>();
+            Clear();
             var length = br.ReadUInt16();
 
             for (var i = 0; i < length; i++)
             {
                 var x = KryptonType<TK>.Read(br);
                 
-                Value.Add(x);
+                Add(x);
             }
         }
 
