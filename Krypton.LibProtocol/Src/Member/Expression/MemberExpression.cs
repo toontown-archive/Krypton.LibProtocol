c9bcd511b70a602779bb82dc0653f6e13db16974
Chandler Stowell <chandler14362@gmail.com>
2017-09-06 13:24:21 -0400

    expressions: add Member and Numerical expressions

---
+++ Krypton.LibProtocol/Src/Member/Expression/MemberExpression.cs
@@ -0,0 +1,16 @@
+﻿using Krypton.LibProtocol.Target;
+
+namespace Krypton.LibProtocol.Member.Expression
+{
+    public class MemberExpression : IExpression, ITemplateType
+    {
+        public string TemplateName => "member_expression";
+        
+        public IMember Member { get; }
+
+        public MemberExpression(IMember member)
+        {
+            Member = member;
+        }
+    }
+}
