b441a523e875d1698c7fc6bcc5b152848396e0a9
Chandler Stowell <chandler14362@gmail.com>
2017-09-06 15:20:39 -0400

    statement: climb through the statement tree while searching

--- Krypton.LibProtocol/Src/Member/Statement/IStatementContainer.cs
+++ Krypton.LibProtocol/Src/Member/Statement/IStatementContainer.cs
@@ -14,6 +14,17 @@ namespace Krypton.LibProtocol.Member.Statement
     {
         public static IStatement FindStatement(IStatementContainer container, Func<IStatement, bool> filter)
         {
+            // Climb through the statement tree if the container has a parent
+            if (container is IStatement statement && statement.Parent != null)
+            {
+                var res = FindStatement(statement.Parent, filter);
+                if (res != null)
+                {
+                    return res;
+                }
+            }
+
+            // Loop through the container and see if any statements match against the filter
             foreach (var s in container.Statements)
             {
                 if (filter(s))
