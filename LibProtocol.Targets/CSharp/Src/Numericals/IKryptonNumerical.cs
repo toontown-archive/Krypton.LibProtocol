52096066124f2f40a5e3c977b89ed4f9a9bc88c8
Chandler Stowell <chandler14362@gmail.com>
2017-09-19 14:18:49 -0400

    c#: numerical code gen template and interface
    KryptonNumericals are defined as value types. The template will be used to minifying each numerical definition.

---
+++ LibProtocol.Targets/CSharp/Src/Numericals/IKryptonNumerical.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Krypton.LibProtocol.Numericals
+{
+    public interface IKryptonNumerical : IComparable, IConvertible, IKryptonType
+    {
+    }
+
+    public interface IKryptonNumerical<T> 
+        : IComparable<T>, IEquatable<T>, IKryptonNumerical
+        where T : struct, IComparable, IComparable<T>, IConvertible, IEquatable<T>
+    {
+    }
+}
